216504244adabcfd5d2216c2b4d96be765c7ce8a
Vialsiu <D00265675@student.dkit.ie>
2025-04-01 22:55:12 +0100

    Crawler can move and only in the grid bound

--- Crawler.cpp
+++ Crawler.cpp
@@ -11,6 +11,32 @@ Crawler::Crawler(int id, int x, int y, Direction dir, int size) {
 }
 
 void Crawler::move() {
+    if (!alive) return;
+    Position newCoord = position;
+
+    switch(direction) {
+        case Direction::North:
+            position.y--;
+        break;
+        case Direction::East:
+            position.x++;
+        break;
+        case Direction::South:
+            position.y++;
+        break;
+        case Direction::West:
+            position.x--;
+        break;
+    }
+    if (newCoord.x >= 0 &&
+        newCoord.x < 10 &&
+        newCoord.y >= 0 &&
+        newCoord.y < 10)
+        {
+        position = newCoord;
+    }
+
+    path.push_back(position);
 }
 
 bool Crawler::isWayBlocked() const {
