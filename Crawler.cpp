b2cc0fe1d34ce61488356d9f752d11b0aeb2123b
Vialsiu <D00265675@student.dkit.ie>
2025-04-01 22:21:01 +0100

    Created project file structure

---
+++ Crawler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fedan on 01/04/2025.
+//
+
+#include "Crawler.h"
---
+++ Crawler.h
@@ -0,0 +1,16 @@
+//
+// Created by fedan on 01/04/2025.
+//
+
+#ifndef CRAWLER_H
+#define CRAWLER_H
+
+
+
+class Crawler {
+
+};
+
+
+
+#endif //CRAWLER_H
