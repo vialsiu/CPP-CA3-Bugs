8e4e4660ce7164bcb751c69074e5bdbf2eb7811a
Iker <eliasarza21@gmail.com>
2025-04-04 00:47:20 +0100

    For Feature 6: Display all Cells
    Add displayAllCells() method to Board for showing bug positions by cell
    
    Implement logic to group and print all crawlers by cell location
    
    Show empty and occupied cells with corresponding bug IDs in output
    
    For Feature 7: Expanded Eat Functionality on Tap
    
    Implement combat logic after movement: biggest bug eats others in same cell
    
    Enhance tapBoard() to detect and declare Last Bug Standing
    
    Force final showdown after 15 taps if multiple bugs still alive

---
+++ cmake-build-debug/CMakeFiles/BugsLife.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/elias/CLionProjects/CPP-CA3-Bugs-lastgit/Board.cpp" "CMakeFiles/BugsLife.dir/Board.cpp.obj" "gcc" "CMakeFiles/BugsLife.dir/Board.cpp.obj.d"
+  "C:/Users/elias/CLionProjects/CPP-CA3-Bugs-lastgit/Crawler.cpp" "CMakeFiles/BugsLife.dir/Crawler.cpp.obj" "gcc" "CMakeFiles/BugsLife.dir/Crawler.cpp.obj.d"
+  "C:/Users/elias/CLionProjects/CPP-CA3-Bugs-lastgit/main.cpp" "CMakeFiles/BugsLife.dir/main.cpp.obj" "gcc" "CMakeFiles/BugsLife.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BugsLife.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "BugsLife.exe"
+  "BugsLife.exe.manifest"
+  "BugsLife.pdb"
+  "CMakeFiles/BugsLife.dir/Board.cpp.obj"
+  "CMakeFiles/BugsLife.dir/Board.cpp.obj.d"
+  "CMakeFiles/BugsLife.dir/Crawler.cpp.obj"
+  "CMakeFiles/BugsLife.dir/Crawler.cpp.obj.d"
+  "CMakeFiles/BugsLife.dir/main.cpp.obj"
+  "CMakeFiles/BugsLife.dir/main.cpp.obj.d"
+  "libBugsLife.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BugsLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
